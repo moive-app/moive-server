166882af115d2aee5165f5c41804edec5c4be480
eujjong18 <eujjong18@naver.com>
2026-08-28 23:59:28 +0900

    [chore #19] Swagger JWT Bearer 인증 스킴 추가

--- src/main/java/com/moive/MoiveBE/global/config/SwaggerConfig.java
+++ src/main/java/com/moive/MoiveBE/global/config/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.moive.MoiveBE.global.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +13,26 @@ import io.swagger.v3.oas.models.info.Info;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "BearerAuth";
+
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER);
+
         return new OpenAPI()
                 .info(new Info()
                         .title("MoiveBE API")
                         .description("MoiveBE API 명세서")
                         .version("v1"))
-                .addServersItem(new Server().url("/"));
+                .addServersItem(new Server().url("/"))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, bearerScheme))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
     }
 
 }
