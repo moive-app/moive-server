96f96a3ec4b767cfdfc785e7a9e2232c9433d92d
준혁 <160326131+Jun3671@users.noreply.github.com>
2026-09-08 03:41:44 +0900

    [feat #31] 모임 Repository 구현

---
+++ src/main/java/com/moive/MoiveBE/domain/meeting/repository/ParticipantRepository.java
@@ -0,0 +1,11 @@
+package com.moive.MoiveBE.domain.meeting.repository;
+
+import com.moive.MoiveBE.domain.meeting.entity.Participant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+
+    Optional<Participant> findByMeetingIdAndUserIdAndLeftAtIsNull(Long meetingId, Long userId);
+}
