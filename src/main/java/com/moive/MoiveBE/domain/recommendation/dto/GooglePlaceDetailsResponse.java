49e200f8ce731ef0e219f5bfa8196a9cfc21d41d
준혁 <160326131+Jun3671@users.noreply.github.com>
2026-09-10 02:58:08 +0900

    [feat #43] GooglePlaces location 필드 추가

--- src/main/java/com/moive/MoiveBE/domain/recommendation/dto/GooglePlaceDetailsResponse.java
+++ src/main/java/com/moive/MoiveBE/domain/recommendation/dto/GooglePlaceDetailsResponse.java
@@ -6,7 +6,8 @@ public record GooglePlaceDetailsResponse(
         LocalizedText displayName,
         LocalizedText primaryTypeDisplayName,
         String formattedAddress,
-        List<Photo> photos
+        List<Photo> photos,
+        Location location
 ) {
     public record LocalizedText(
             String text,
@@ -16,5 +17,10 @@ public record GooglePlaceDetailsResponse(
 
     public record Photo(
             String name
+    ) {}
+
+    public record Location(
+            Double latitude,
+            Double longitude
     ) {}
 }
