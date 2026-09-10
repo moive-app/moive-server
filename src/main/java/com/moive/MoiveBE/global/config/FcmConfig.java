a01210477295dda7ba86efc1a0eca760fb12964e
준혁 <160326131+Jun3671@users.noreply.github.com>
2026-09-11 02:29:38 +0900

    [fix #49] 테스트 환경에서 FCM 초기화 제외 (@Profile("!test"))

--- src/main/java/com/moive/MoiveBE/global/config/FcmConfig.java
+++ src/main/java/com/moive/MoiveBE/global/config/FcmConfig.java
@@ -5,12 +5,14 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import jakarta.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
+@Profile("!test")
 public class FcmConfig {
 
     @Value("${firebase.service-account-path}")
