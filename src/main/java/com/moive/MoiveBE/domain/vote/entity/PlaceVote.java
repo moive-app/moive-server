58ac4ce216453c83883460c160be8746bceb69f9
eujjong18 <eujjong18@naver.com>
2026-09-11 17:08:25 +0900

    [feat #48] PlaceVote 엔티티 추가

---
+++ src/main/java/com/moive/MoiveBE/domain/vote/entity/PlaceVote.java
@@ -0,0 +1,48 @@
+package com.moive.MoiveBE.domain.vote.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "place_votes")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PlaceVote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "place_vote_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Long meetingId;
+
+    @Column(nullable = false)
+    private Long participantId;
+
+    @Column(nullable = false)
+    private Long recommendedPlaceId;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
