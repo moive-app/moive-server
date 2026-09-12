a3a1fcd8ebb19da444c81d360eb2797f9503950c
eujjong18 <eujjong18@naver.com>
2026-09-12 17:04:44 +0900

    [feat #71] 장소 투표 마감 기한 경과 판정 및 장소 확정 로직 추가 - 장소 투표 마감 기한 환경변수 추가 (default: 1일)

--- src/main/java/com/moive/MoiveBE/domain/meeting/repository/MeetingRepository.java
+++ src/main/java/com/moive/MoiveBE/domain/meeting/repository/MeetingRepository.java
@@ -1,8 +1,10 @@
 package com.moive.MoiveBE.domain.meeting.repository;
 
 import com.moive.MoiveBE.domain.meeting.entity.Meeting;
+import com.moive.MoiveBE.domain.meeting.entity.MeetingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
@@ -10,4 +12,7 @@ public interface MeetingRepository extends JpaRepository<Meeting, Long> {
     Optional<Meeting> findByInviteCode(String inviteCode);
 
     boolean existsByInviteCode(String inviteCode);
+
+    List<Meeting> findAllByStatus(MeetingStatus status);
+
 }
