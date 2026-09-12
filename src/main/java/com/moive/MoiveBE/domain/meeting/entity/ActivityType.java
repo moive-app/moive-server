9ec92f828ae6c660601b2f8acdb26498f7f15555
준혁 <160326131+Jun3671@users.noreply.github.com>
2026-09-12 16:33:41 +0900

    [refactor #68] ActivityType enum 디자인 기준으로 수정

--- src/main/java/com/moive/MoiveBE/domain/meeting/entity/ActivityType.java
+++ src/main/java/com/moive/MoiveBE/domain/meeting/entity/ActivityType.java
@@ -12,22 +12,21 @@ public enum ActivityType {
     KARAOKE("노래방"),
     PC_ROOM("PC방"),
     COMIC_CAFE("만화카페"),
+    MOVIE("영화관"),
+    PERFORMANCE("공연"),
+    EXHIBITION("전시·미술관"),
+    MUSEUM("박물관"),
     KOREAN_FOOD("한식"),
     WESTERN_FOOD("양식"),
     CHINESE_FOOD("중식"),
     JAPANESE_FOOD("일식"),
     MEAT("고기"),
     SEAFOOD("해산물"),
+    CAFE_DESSERT("카페·디저트"),
     PARK("공원"),
     WALK("산책"),
     HIKING("등산"),
-    SHOPPING("쇼핑"),
-    CAFE("카페"),
-    DESSERT("디저트"),
-    IZAKAYA("이자카야"),
-    COCKTAIL_BAR("칵테일바"),
-    MOVIE("영화"),
-    EXHIBITION("전시/미술관");
+    SHOPPING("쇼핑");
 
     private final String label;
 }
